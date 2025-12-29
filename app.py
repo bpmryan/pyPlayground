3edaadd5fb6dfdce6c7190f8746cce67e8124ca7
bpmryan <bpm_ryand2428@yahoo.com>
2025-12-29 12:11:46 -0600

    Lesson 2

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 # Monday 12/29/25 start
+
+# Lesson 1
 print('First python program')
 print('o----')
 print(' ||||')
 print('*' * 10)
+
+#Lesson 2
+price = 10 # int
+rating = 4.9 # floating point number 
+name = 'Bryan' # string 
+is_published = False # boolean (case sensitive)
+print(price)
