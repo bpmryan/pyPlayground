9e77713f22d2e955de34f37c672a864f0462704b
bpmryan <bpm_ryand2428@yahoo.com>
2025-12-29 12:16:49 -0600

    Split lessons in app.py into different files based off lesson

---
+++ lesson1.py
@@ -0,0 +1,5 @@
+# Lesson 1
+print('First python program')
+print('o----')
+print(' ||||')
+print('*' * 10)
---
+++ lesson3.py
@@ -0,0 +1,4 @@
+# Lesson 3
+# Function that receives user input
+name = input('What is your name? ')
+print('Hi ' + name)
